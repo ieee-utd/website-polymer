e56f02c0e184f8ba72c21dbdfdb608df57fd042e
Caleb Fung <caleb.t.fung@gmail.com>
2018-05-11 21:01:03 -0500

    Remove docs

--- docs/src/notfound.js
+++
@@ -1,7 +0,0 @@
-import{PolymerElement,html}from"../node_modules/@polymer/polymer/polymer-element.js";class NotFoundPage extends PolymerElement{static get properties(){return{}}constructor(){super()}ready(){super.ready()}static get template(){return html`
-      <style>
-        
-      </style>
-
-      <div>404</div>
-    `}}customElements.define("page-notfound",NotFoundPage);
