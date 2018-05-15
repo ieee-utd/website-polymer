38f1e2854e2b22b44874be80e9955e748910eee4
Caleb Fung <caleb.t.fung@gmail.com>
2018-05-14 22:55:16 -0500

    update 404

--- src/notfound.js
+++ src/notfound.js
@@ -19,10 +19,40 @@ class NotFoundPage extends PolymerElement {
   static get template () {
     return html`
       <style>
-        
+        .container {
+          min-height: calc(100vh - 48px);
+          display: flex;
+          flex-direction: column;
+        }
+        .body {
+          flex-grow: 1;
+          min-height: 350px;
+          justify-content: center;
+          background: #f7f7f7;
+          display: flex;
+        }
+        .inner {
+          margin: 10px 24px;
+          width: 1200px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+        a {
+          color: #3488ff;
+          text-decoration: none;
+          border-bottom: 1px #3488ff dotted;
+        }
       </style>
 
-      <div>404</div>
+      <div class="container">
+        <div class="body">
+          <div class="inner">
+            <p>Sorry, we couldn't find what you were looking for.<br/> Head <a href="/home">home</a>.</p>
+          </div>
+        </div>
+        <app-footer></app-footer>
+      </div>
     `;
   }
 }
