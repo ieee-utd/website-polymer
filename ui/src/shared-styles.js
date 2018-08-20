f3fcc8f927cb12fcd12bfa5685487a5bdf7e333d
Arthur Pachachura <pachachura.arthur@gmail.com>
2018-08-20 14:30:48 -0500

    Add fonts

--- ui/src/shared-styles.js
+++ ui/src/shared-styles.js
@@ -4,7 +4,23 @@ const $_documentContainer = document.createElement('template');
 $_documentContainer.innerHTML = `<dom-module id="shared-styles">
   <template>
     <style>
+      :host {
+        --color-primary: #C75B12; //orange
+        --color-background: #212121; //dark grey
+        --color-secondary: #424242; //grey
+        --color-secondary-active: #666666; //lighter grey
 
+        --font-head: "Rubik";
+        --font-main: "Roboto";
+      }
+
+      h1, h2, h3, h4, h5, h6 {
+        font-weight: 700;
+        font-family: var(--font-head);
+      }
+      p {
+        font-family: var(--font-main);
+      }
     </style>
   </template>
 </dom-module>`;
