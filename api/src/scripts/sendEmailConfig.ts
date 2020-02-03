f914b70bf947c655f5070c9f4594ecb4d60f8413
Arthur Pachachura <pachachura.arthur@gmail.com>
2020-02-02 18:01:23 -0600

    Fix build

---
+++ api/src/scripts/sendEmailConfig.ts
@@ -0,0 +1,29 @@
+export const bccEmails = [
+
+];
+
+export const fromEmail = {
+  email: "aap160030@utdallas.edu",
+  name: "Arthur Pachachura"
+}
+
+export const ccEmails = [{
+  email: "vxp170009@utdallas.edu",
+  name: "Vedansh Patel"
+}]
+
+export const replyTo = fromEmail;
+
+export const subject = "Your invitation to apply to The Forge";
+
+export const bodyHtml = `<p>Hello!</p><br>
+
+<p>Thank you for expressing interest in The Forge, a division of IEEEUTD! Based on your response to the interest form, <b>we would like to invite you to complete our <a href="https://utdallas.qualtrics.com/jfe/form/SV_1RgpraesUnQ6cex">application to join The Forge</a></b>. The application takes about 10 minutes to complete and will close as soon as all projects are filled.</p><br>
+
+<p>The Forge is a multidisciplinary project-focused innovation hub focusing on developing the next generation of creative and technical leaders while solving engineering challenges in the community. For more information about us, go to <a href="https://ieeeutd.org/forge">ieeeutd.org/forge</a>. The packet in the application also contains additional information about our policies and the projects available.</p><br>
+
+<p>Your application link: <a href="https://utdallas.qualtrics.com/jfe/form/SV_1RgpraesUnQ6cex">https://utdallas.qualtrics.com/jfe/form/SV_1RgpraesUnQ6cex</a></p><br>
+
+<p>Please email us back if you have any questions. Thank you for your time!</p>`
+
+export const summary = "Based on your response to the interest form, we would like to invite you to complete our application to join The Forge! The application takes about 10 minutes to complete and will close as soon as all projects are filled.";
